af685d75317592f7e880ec16751dd34ce2adaec4
VidushanH <68732910+VidushanH@users.noreply.github.com>
2020-09-28 22:52:55 +0530

    Refactored conflicting files

--- app/src/main/java/com/example/daytoday/DebtListActivity.java
+++ app/src/main/java/com/example/daytoday/DebtListActivity.java
@@ -29,7 +29,7 @@ public class DebtListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_debtlist);
 
 
 
